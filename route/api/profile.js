1ed97bd90e1fea3d904134853522f55ce16aa964
Shakib448 <shakiba448@gmail.com>
2020-10-11 11:30:27 +0600

    Profile Route Updated

--- route/api/profile.js
+++ route/api/profile.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-// @route   GET api/Profile
-// @desc   TEST route
-// @access Public
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-router.get("/", (req, res) => {
-  res.send("Profile Route");
+// @route   GET api/Profile/me
+// @desc   Get current user profile
+// @access Private
+
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]); // This id is getting the profile model schema populate is query
+
+    // not profile
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
