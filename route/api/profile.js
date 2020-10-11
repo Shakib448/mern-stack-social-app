2a0255b08dedb4380c9b068bf9ff215ec6224374
Shakib448 <shakiba448@gmail.com>
2020-10-11 17:33:30 +0600

    Update experience added

--- route/api/profile.js
+++ route/api/profile.js
@@ -164,4 +164,49 @@ router.delete("/", auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/Profile/experience
+// @desc   Add Profile experience
+// @access private
+
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "Title is required").not().isEmpty(),
+      check("company", "Company is required").not().isEmpty(),
+      check("from", "From date is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newExp = { title, company, location, from, to, description, current };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
 const cors = require("cors");
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 5000;
 
@@ -14,7 +14,7 @@ app.use(cors());
 
 // init Middleware
 app.use(express.json({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("API running");
