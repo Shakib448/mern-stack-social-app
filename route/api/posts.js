f0256576fc06286b8f84fda81024c460a0b16ac0
Shakib448 <shakiba448@gmail.com>
2020-11-08 16:28:37 +0600

    Like Function added

--- route/api/posts.js
+++ route/api/posts.js
@@ -106,4 +106,30 @@ router.delete("/:id", auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/Post/like/:id
+// @desc   Like a post
+// @access Private
+
+router.put("/likes/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post has already been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(404).json({ msg: "Post already liked" });
+    }
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
