70763a8013fdf3d7134ef6cb127f7f8195097539
Shakib448 <shakiba448@gmail.com>
2020-12-03 10:26:42 +0600

    add Post added

--- client/src/Components/Posts/PostItem.js
+++ client/src/Components/Posts/PostItem.js
@@ -15,7 +15,7 @@ const PostItem = ({
   return (
     <div className="post bg-white p-1 my-1">
       <div>
-        <Link to={`/profile/${_id}`}>
+        <Link to={`/profile/${user}`}>
           <img className="round-img" src={avatar} alt="" />
           <h4>{name}</h4>
         </Link>
