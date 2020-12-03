bb46cd347c48315cb157397e9746ea3abdeb637e
Shakib448 <shakiba448@gmail.com>
2020-12-03 22:09:17 +0600

    heroku push

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const connectDB = require("./config/db");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 
@@ -16,15 +17,20 @@ app.use(cors());
 app.use(express.json({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("API running");
-});
-
 // Define routes
 
 app.use("/api/users", require("./route/api/users"));
 app.use("/api/auth", require("./route/api/auth"));
 app.use("/api/profile", require("./route/api/profile"));
 app.use("/api/posts", require("./route/api/posts"));
+
+// Server static assets in production
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server is started on port ${PORT}`));
