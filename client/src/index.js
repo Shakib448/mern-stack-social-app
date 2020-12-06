7c7f0397d17194d7d4eef7060823bb302deb96fc
Shakib448 <shakiba448@gmail.com>
2020-12-06 21:19:42 +0600

    Img added

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store/store";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
